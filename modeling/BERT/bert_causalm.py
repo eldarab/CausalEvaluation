4b9a4718a4cbc0b93d93357cc42d7451e6e78af2
Eldar Abraham <53745147+eldarab@users.noreply.github.com>
2021-06-20 16:59:19 +0300

    added BertCausalmForSequenceClassification

--- modeling/BERT/bert_causalm.py
+++ modeling/BERT/bert_causalm.py
@@ -3,12 +3,12 @@ from typing import Optional, Tuple
 
 import torch
 import torch.nn as nn
-from torch.nn import CrossEntropyLoss
-from transformers import BertConfig
+from torch.nn import CrossEntropyLoss, MSELoss, BCEWithLogitsLoss
 from transformers.file_utils import ModelOutput
+from transformers.modeling_outputs import SequenceClassifierOutput
 from transformers.models.bert.modeling_bert import BertPreTrainedModel, BertModel, BertLMPredictionHead
 
-from models.BERT.configuration_causalm import BertCausalmConfig, CausalmHeadConfig
+from modeling.BERT.configuration_causalm import BertCausalmConfig, CausalmHeadConfig
 from utils import SEQUENCE_CLASSIFICATION, TOKEN_CLASSIFICATION
 
 
@@ -131,6 +131,107 @@ class BertForCausalmAdditionalPreTraining(BertPreTrainedModel):
             mlm_logits=mlm_head_scores,
             tc_logits=tc_heads_scores,
             cc_logits=cc_heads_scores,
+            hidden_states=outputs.hidden_states,
+            attentions=outputs.attentions,
+        )
+
+
+class BertCausalmForSequenceClassification(BertPreTrainedModel):
+    config_class = BertCausalmConfig
+    base_model_prefix = "bert_causalm"
+
+    def __init__(self, config: BertCausalmConfig):
+        super().__init__(config)
+        self.num_labels = config.num_labels
+        self.config = config
+
+        if self.config.sequence_classifier_type not in {'task', 'cc', 'tc'}:
+            raise RuntimeError(f'Illegal sequence_classifier_type {self.config.sequence_classifier_type}')
+        self.classifier_type = self.config.sequence_classifier_type
+
+        self.bert = BertModel(config)
+        self.dropout = nn.Dropout(config.hidden_dropout_prob)
+        self.classifier = nn.Linear(config.hidden_size, config.num_labels)
+
+        self.init_weights()
+
+    def forward(
+        self,
+        input_ids=None,
+        attention_mask=None,
+        token_type_ids=None,
+        position_ids=None,
+        head_mask=None,
+        inputs_embeds=None,
+        task_label=None,
+        cc_label=None,
+        tc_label=None,
+        output_attentions=None,
+        output_hidden_states=None,
+        return_dict=None,
+    ):
+        r"""
+        labels (:obj:`torch.LongTensor` of shape :obj:`(batch_size,)`, `optional`):
+            Labels for computing the sequence classification/regression loss. Indices should be in :obj:`[0, ...,
+            config.num_labels - 1]`. If :obj:`config.num_labels == 1` a regression loss is computed (Mean-Square loss),
+            If :obj:`config.num_labels > 1` a classification loss is computed (Cross-Entropy).
+        """
+        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+
+        outputs = self.bert(
+            input_ids,
+            attention_mask=attention_mask,
+            token_type_ids=token_type_ids,
+            position_ids=position_ids,
+            head_mask=head_mask,
+            inputs_embeds=inputs_embeds,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict,
+        )
+
+        pooled_output = outputs[1]
+
+        pooled_output = self.dropout(pooled_output)
+        logits = self.classifier(pooled_output)
+
+        labels = None
+        if self.classifier_type == 'task':
+            labels = task_label
+        elif self.classifier_type == 'cc':
+            labels = cc_label
+        elif self.classifier_type == 'tc':
+            labels = tc_label
+
+        loss = None
+        if labels is not None:
+            if self.config.problem_type is None:
+                if self.num_labels == 1:
+                    self.config.problem_type = "regression"
+                elif self.num_labels > 1 and (labels.dtype == torch.long or labels.dtype == torch.int):
+                    self.config.problem_type = "single_label_classification"
+                else:
+                    self.config.problem_type = "multi_label_classification"
+
+            if self.config.problem_type == "regression":
+                loss_fct = MSELoss()
+                if self.num_labels == 1:
+                    loss = loss_fct(logits.squeeze(), labels.squeeze())
+                else:
+                    loss = loss_fct(logits, labels)
+            elif self.config.problem_type == "single_label_classification":
+                loss_fct = CrossEntropyLoss()
+                loss = loss_fct(logits.view(-1, self.num_labels), labels.view(-1))
+            elif self.config.problem_type == "multi_label_classification":
+                loss_fct = BCEWithLogitsLoss()
+                loss = loss_fct(logits, labels)
+        if not return_dict:
+            output = (logits,) + outputs[2:]
+            return ((loss,) + output) if loss is not None else output
+
+        return SequenceClassifierOutput(
+            loss=loss,
+            logits=logits,
             hidden_states=outputs.hidden_states,
             attentions=outputs.attentions,
         )
