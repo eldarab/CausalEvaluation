8a9f9c4767d22bd7854d68c311e317709175375d
Eldar Abraham <53745147+eldarab@users.noreply.github.com>
2021-07-19 10:19:22 +0300

    make metrics static

--- utils/metrics.py
+++ utils/metrics.py
@@ -1,3 +1,5 @@
+import warnings
+
 import numpy as np
 from sklearn.metrics import accuracy_score
 from torch import no_grad
@@ -18,81 +20,72 @@ def calc_accuracy_from_logits(outputs, true_labels, model):
 
 
 class CausalmMetrics:
-    def __init__(self, tokenizer_checkpoint, text_key='text', device=DEVICE):
-        self.tokenizer = BertTokenizerFast.from_pretrained(tokenizer_checkpoint)
-        self.device = device
-        self.text_key = text_key
-        # todo dataloader instead of dataset iterator
+    def __init__(self):
+        # TODO dataloader instead of dataset iterator
+        pass
 
-    def __compute_class_expectation(self, model, dataset, cls=0):
+    @staticmethod
+    def __compute_class_expectation(model, dataset, cls):
         model.eval()
         with no_grad():
             expectation = 0.
             for example in dataset:
-                inputs = self.tokenizer(example[self.text_key], padding=True, truncation=True, return_tensors='pt')
-                inputs.to(self.device)
-
-                outputs = model(**inputs)
-
-                probas = softmax(outputs.logits, dim=-1)
-
-                expectation += mean(probas, dim=0)[cls].item()  # / len(dataloader)
-
+                outputs = model(input_ids=example['input_ids'], attention_mask=example['attention_mask'], token_type_ids=example['token_type_ids'])
+                cls_probabilities = softmax(outputs.logits, dim=-1)
+                expectation += mean(cls_probabilities, dim=0)[cls].item()  # / len(dataloader)
             return expectation / len(dataset)
 
-    def conexp(self, model, dataset) -> float:
-        dataset_0 = dataset.filter(lambda example: example['tc_labels'] == 0)
-        dataset_1 = dataset.filter(lambda example: example['tc_labels'] == 1)
+    @staticmethod
+    def conexp(model, dataset, tc_indicator_name, cls=0) -> float:
+        # TODO generalize to non-binary concept indicator
+        dataset_0 = dataset.filter(lambda example: example[tc_indicator_name] == 0)
+        dataset_1 = dataset.filter(lambda example: example[tc_indicator_name] == 1)
 
-        e_0 = self.__compute_class_expectation(model, dataset_0, cls=0)
-        e_1 = self.__compute_class_expectation(model, dataset_1, cls=0)
-
+        e_0 = CausalmMetrics.__compute_class_expectation(model, dataset_0, cls)
+        e_1 = CausalmMetrics.__compute_class_expectation(model, dataset_1, cls)
         conexp = abs(e_1 - e_0)
 
         return conexp
 
-    def treate(self, model_o, model_cf, dataset, cls=0) -> float:
+    @staticmethod
+    def treate(model_o, model_cf, dataset, cls=0) -> float:
         model_o.eval()
         model_cf.eval()
 
         with no_grad():
             treate = 0.
             for example in dataset:
-                inputs = self.tokenizer(example[self.text_key], padding=True, truncation=True, return_tensors='pt')
-                inputs.to(self.device)
+                outputs_o = model_o(input_ids=example['input_ids'], attention_mask=example['attention_mask'],
+                                    token_type_ids=example['token_type_ids'])
+                outputs_cf = model_cf(input_ids=example['input_ids'], attention_mask=example['attention_mask'],
+                                      token_type_ids=example['token_type_ids'])
 
-                outputs_o = model_o(**inputs)
-                outputs_cf = model_cf(**inputs)
+                cls_probabilities_o = softmax(outputs_o.logits, dim=-1)
+                cls_probabilities_cf = softmax(outputs_cf.logits, dim=-1)
 
-                probas_o = softmax(outputs_o.logits, dim=-1)
-                probas_cf = softmax(outputs_cf.logits, dim=-1)
-
-                treate += pt_abs(probas_o - probas_cf)[:, cls].sum().item()
+                treate += pt_abs(cls_probabilities_o - cls_probabilities_cf)[:, cls].sum().item()
 
             treate /= len(dataset)
 
         return treate
 
-    def ate(self, model, dataset_f, dataset_cf, cls=0) -> float:
+    @staticmethod
+    def ate(model, dataset_f, dataset_cf, cls=0) -> float:
         assert len(dataset_f) == len(dataset_cf)
         model.eval()
 
         with no_grad():
             ate = 0.
             for example_f, example_cf in zip(dataset_f, dataset_cf):
-                inputs_f = self.tokenizer(example_f[self.text_key], padding=True, truncation=True, return_tensors='pt')
-                inputs_cf = self.tokenizer(example_cf[self.text_key], padding=True, truncation=True, return_tensors='pt')
+                outputs_f = model(input_ids=example_f['input_ids'], attention_mask=example_f['attention_mask'],
+                                  token_type_ids=example_f['token_type_ids'])
+                outputs_cf = model(input_ids=example_cf['input_ids'], attention_mask=example_cf['attention_mask'],
+                                   token_type_ids=example_cf['token_type_ids'])
 
-                inputs_f.to(self.device)
-                inputs_cf.to(self.device)
+                cls_probabilities_f = softmax(outputs_f.logits, dim=-1)
+                cls_probabilities_cf = softmax(outputs_cf.logits, dim=-1)
 
-                outputs_f = model(**inputs_f)
-                outputs_cf = model(**inputs_cf)
-
-                probas_f = softmax(outputs_f.logits, dim=-1)
-                probas_cf = softmax(outputs_cf.logits, dim=-1)
-
-                ate += pt_abs(probas_f - probas_cf)[:, cls].sum().item()
+                ate += pt_abs(cls_probabilities_f - cls_probabilities_cf)[:, cls].sum().item()
 
             ate /= len(dataset_f)
 
